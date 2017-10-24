e231f1a865568ed1724bfacd6b68d81ad1cdf821
JJLionHeart <jj.lopezjaimez@gmail.com>
2017-10-24 16:55:39 -0500

    test push

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
-<?php
-    echo 'hello world :)';
-?>
+<html>
+    <head>
+        <title> Web project </title>
+    </head>
+    <body>
+        <center><h1>Hello world!</h1></center>
+    </body>
+</html>
