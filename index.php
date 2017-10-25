f873fe9d55a87b61c6dd618a8ff125475b7824df
JJLionHeart <jj.lopezjaimez@gmail.com>
2017-10-25 14:12:03 -0500

    add initial stile to index.php

--- index.php
+++ index.php
@@ -1,13 +1,34 @@
 <html>
-    <head>
-        <title> Web project </title>
-         <link rel="stylesheet" type="text/css" href="mystyle.css"/>
-    </head>
-    <body>
-        <div class="header">
-        <span class="header">
-            <center> Web page title goes here </center>
-        </span>
-        </div>
-    </body>
+	<head>
+		<title> Web project </title>
+		<link rel="stylesheet" type="text/css" href="mystyle.css"/>
+	</head>
+	<body>
+		<div class="rootContainer">
+			<div class="header">
+				<span class="header">
+					
+						<h1> Web page title goes here</h1>
+			     </span>
+			</div>
+			<div class="login">
+				<center><h1>Login</h1></center>
+				<fieldset>
+					<form>
+						<p>
+							<b>Username: </b>
+						</p>
+						<input type="text" value="username" class="loginUser"/>
+						<p>
+							<b>Password:</b>
+						</p>
+						<input type="password" class="loginPassword"/>
+						<center>
+							<input type="submit" value="Login"/>
+						</center>
+					</form>
+				</fieldset>
+			</div>
+		</div>
+	</body>
 </html>
