cc13f87706bfe9691be522ef3fae35464d7aeb33
JJLionHeart <jj.lopezjaimez@gmail.com>
2017-10-24 20:30:11 -0500

    add style

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
-<?php
-
-    function connection_string() {
-        extract(parse_url($_ENV["DATABASE_URL"]));
-        return "user=$user password=$pass host=$host dbname=". substr($path, 1);
-    }   
-    
-    $pg_conn = pg_connect(connection_string());
-
-    $result = pg_query($pg_conn, "INSERT INTO Users(id, name) VALUES('aa', 'wea')");
-    
-    
-?>
 <html>
     <head>
         <title> Web project </title>
+         <link rel="stylesheet" type="text/css" href="mystyle.css"/>
     </head>
     <body>
         <center><h1>Hello world!</h1></center>
