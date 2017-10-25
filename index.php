325cf69d46f77b5acec18319c30a9877e7b4cf6b
JJLionHeart <jj.lopezjaimez@gmail.com>
2017-10-24 20:20:14 -0500

    postgress test

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+
+    function connection_string() {
+        extract(parse_url($_ENV["DATABASE_URL"]));
+        return "user=$user password=$pass host=$host dbname=". substr($path, 1);
+    }   
+    
+    $pg_conn = pg_connect(connection_string());
+
+    $result = pg_query($pg_conn, "CREATE TABLE Users(id varchar(10), name varchar(10))");
+    
+?>
 <html>
     <head>
         <title> Web project </title>
