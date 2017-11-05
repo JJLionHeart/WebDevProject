fcdb683b56bb6aa4447c77e56dbeb91f04e7014e
Juan Jose Lopez Jaimez <jj.lopezjaimez@gmail.com>
2017-11-05 15:41:58 -0600

    add functionality to register

--- data/dataLayer.php
+++ data/dataLayer.php
@@ -5,6 +5,7 @@ function databaseConnection()
         extract(parse_url($_ENV["DATABASE_URL"]));
         $pg_string =  "user=$user password=$pass host=$host dbname=" . substr($path, 1);
         $pg_conn = pg_connect($pg_string);
+        return $pg_conn;
 }
 
 function attemptLogin($userName, $userPassword)
