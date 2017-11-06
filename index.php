83ed5b0eeb031054ce35de7a40d35daa2478bb0d
JJLionHeart <jj.lopezjaimez@gmail.com>
2017-11-06 07:42:49 -0600

    logout implemented

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+        if(isset($_SESSION['Username']) || $_SESSION['Username'] != '') {
+            header("Location: home.php");
+            die();
+        }
+?>
 <html>
 	<head>
 		<title> Web project </title>
