4347a08d06a1cacc4563b5907dae65c06b6ac654
JJLionHeart <jj.lopezjaimez@gmail.com>
2017-11-06 23:38:56 -0600

    modify database

--- data/database.sql
+++ data/database.sql
@@ -3,3 +3,19 @@ CREATE TABLE Users(username varchar(30) PRIMARY KEY,
                   first_name varchar(30) NOT NULL,
                   last_name varchar(30) NOT NULL,
                   email varchar(30));
+
+CREATE TABLE Projects(project_id serial PRIMARY KEY, name varchar(50), 
+                      description varchar(256), 
+                      deadline DATE);
+
+CREATE TABLE UsersAndProjects(project_id INTEGER REFERENCES Projects(project_id), 
+                              username varchar(30) REFERENCES Users(username),
+                              PRIMARY KEY (project_id, username));
+
+                        
+CREATE TABLE Tasks(taskid serial PRIMARY KEY, content varchar(30), dealine DATE,
+                   username varchar(30) REFERENCES Users(username));
+
+CREATE TABLE Contacts(username varchar(30) REFERENCES Users(username), 
+                      contact varchar(30) REFERENCES Users(username),
+                      PRIMARY KEY(username, contact));
