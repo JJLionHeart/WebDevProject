2e0a4a7f0fabee3d641ceb48dfb67f3c42d08ec3
Barbara Valdez <babe-hime@hotmail.com>
2017-11-08 15:28:06 -0600

    changes

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 			</div>
 		</div>
 		<div class="card-panel col s4 z-depth-4 offset-s4">
-			<center><h4>Sign in</h4></center>
+			<center><h4 class="deep-purple-text">Sign in</h4></center>
 			<fieldset>
 			<form class= "login-form">
 				<div class="row margin">
@@ -36,66 +36,65 @@
 						<input id="password-login" type="password">
 						<label for="password-login" class="center-align deep-purple-text">Password</label>
 					</div>
-					</div>
-				<center>
-					<input type="submit" class="btn-large waves-effect waves-light deep-purple" value="Enter"/>
-				</center>
+				</div>
 			</form>
 			</fieldset>
+			</br>
+			<a id="submit" class="btn-large waves-effect waves-light deep-purple col s4 offset-s4">Enter</a>
+			</br>
 		</div>
 	</div>
 
 <div id="index-banner" class="parallax-container white">
-<div class="section no-pad-bot">
-	<div class="container">
-		<br><br>
-		<h1 class="header center teal-text text-lighten-2">Welcome to our web page </h1>
-		<div class="row center">
-			<h5 class="header col s12 light">Create an account</h5>
+	<div class="section no-pad-bot">
+		<div class="container">
+			<br><br>
+			<h1 class="header center teal-text text-lighten-2">Welcome to our web page </h1>
+			<div class="row center">
+				<h5 class="header col s12 light">Create an account</h5>
+			</div>
 		</div>
 	</div>
 </div>
-</div>
 <div id= "register" class="container white">
-<div class="section">
-	<fieldset>
-		<legend>Register your account</legend>
-		<br>
-		<p>First Name<br/>
-			<input id="firstName" type="text">
-			<span id="errorFirstName"></span>
-		</p>
+	<div class="section">
+		<fieldset>
+			<legend>Register your account</legend>
+			<br>
+			<p>First Name<br/>
+				<input id="firstName" type="text">
+				<span id="errorFirstName"></span>
+			</p>
 
-		<p>Last Name<br/>
-			<input id="lastName" type="text">
-			<span id="errorLastName"></span>
-		</p>
+			<p>Last Name<br/>
+				<input id="lastName" type="text">
+				<span id="errorLastName"></span>
+			</p>
 
-		<p>Username<br/>
-			<input id="userName" type="text">
-			<span id="errorUserName"></span>
-		</p>
+			<p>Username<br/>
+				<input id="userName" type="text">
+				<span id="errorUserName"></span>
+			</p>
 
-		<p>Email<br/>
-			<input id="email" type="text">
-			<span id="errorEmail"></span>
-		</p>
+			<p>Email<br/>
+				<input id="email" type="text">
+				<span id="errorEmail"></span>
+			</p>
 
-		<p>Password<br/>
-			<input id="password" type="password">
-			<span id="errorPassword"></span>
-		</p>
+			<p>Password<br/>
+				<input id="password" type="password">
+				<span id="errorPassword"></span>
+			</p>
 
-		<p>Password confirmation<br/>
-			<input id="password2" type="password">
-			<span id="errorPassword2"></span>
-		</p>
-	</fieldset>
-</div>
+			<p>Password confirmation<br/>
+				<input id="password2" type="password">
+				<span id="errorPassword2"></span>
+			</p>
+		</fieldset>
+	</div>
 </div>
 <div class="row center">
 		<a id="submitBtn" class="btn-large waves-effect waves-light ">Register</a>
-</div>
 </div>
 	<script type="text/javascript" src="js/jquery.js"></script>
 	<script type="text/javascript" src="js/materialize.min.js"></script>
