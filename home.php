4861f17eefb0ebd43ad6859f2e7378616732c987
Barbara Valdez <babe-hime@hotmail.com>
2017-11-12 03:04:02 -0600

    Connecting frontend

--- home.php
+++ home.php
@@ -99,7 +99,8 @@
 	<script type="text/javascript" src="js/materialize.min.js"></script>
     <script type="text/javascript" src="js/home.js"></script>
     <script src="js/materialize-collection-actions-1.0.0.min.js"></script>
-    <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
 </body>
 
 </html>
