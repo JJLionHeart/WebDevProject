a886ecbccb03057b826a8dbfb1c776388c4a09a0
Barbara Valdez <babe-hime@hotmail.com>
2017-11-12 23:41:41 -0600

    Connecting frontend

--- agenda.php
+++ agenda.php
@@ -32,8 +32,8 @@
             <div class="col s4">
                 <form>
                     <div class="input-field">
-                        <input id="search" type="search" required>
-                        <label id="search-icon" class="label-icon" placeholder="Search firstname + lastname" for="search"><i class="material-icons">search</i></label>
+                        <input id="search" type="search" placeholder="Search firstname + lastname" required>
+                        <label id="search-icon" class="label-icon" for="search"><i class="material-icons">search</i></label>
                         <i class="material-icons">close</i>
                     </div>
                 </form>
@@ -49,7 +49,7 @@
         </div>    
     </nav>
 
-    <div class= "addcontacts row">
+    <div class= "addcontacts section">
         <ul id="contactsInfo" class="collection">
 
         </ul> 
